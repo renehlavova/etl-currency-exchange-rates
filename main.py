527024fefb8dbde200c90b9ad327155541bb1cde
Renata Hlavova <hlavova.renata@gmail.com>
2023-08-05 16:05:27 +0200

    add main entrypoint

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-"""Main entrypoint for the template package"""
+"""Main entrypoint for the etl-currency-exchange-rate package"""
+
 import logging
+
+from etl_currency_exchange_rates.ecb_client import ECBClient
 
 logger = logging.getLogger(__name__)
 
 
 def main():
     """Main entrypoint"""
-    logging.basicConfig(level=logging.INFO)
-    if __debug__:
-        logging.basicConfig(level=logging.DEBUG, force=True)
-    # Add your actual code here
-    logger.info("This package does nothing")
+
+    client = ECBClient()
+    base_currencies = [
+        "PLN",
+        "NOK",
+        "RON",
+        "ISK",
+        "SEK",
+        "CHF",
+        "CZK",
+        "TRY",
+        "BGN",
+        "HUF",
+        "DKK",
+        "GBP",
+        "USD",
+        "CAD",
+        "AUD",
+    ]
+
+    for base_currency in base_currencies:
+        print(client.get_currency_exchange_rate(base_currency, start_date="2023-07-21"))
 
 
 if __name__ == "__main__":
